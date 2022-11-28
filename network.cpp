8783fad1938228a526bb3d3f9862bee8cd232d23
Abhishek <45958856+abhishekprakash256@users.noreply.github.com>
2022-11-27 20:56:10 -0500

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -1,3 +1,14 @@
+/* 
+The main file contains the network and the data loader 
+The trainer code and the model evaluation on the test data
+The code is taken from the https://github.com/pytorch/examples/blob/main/cpp/mnist/mnist.cpp and build  on top of it
+The offical documentation code https://pytorch.org/tutorials/advanced/cpp_frontend.html
+
+*/
+
+/*
+The imports 
+*/
 #include <torch/torch.h>
 
 #include <cstddef>
@@ -9,34 +20,146 @@
 using namespace std ;
 
 // Where to find the MNIST dataset.
-//const char* kDataRoot = "./data";
 const char* kDataRoot = "/home/abhishek/Personal/dbms_project/pytorch_cpp/exp/fashion-mnist";  // the data path for fashion mnsit 
 
 // The batch size for training.
-const int64_t kTrainBatchSize = 64;
+const int64_t kTrainBatchSize = 32;
 
 // The batch size for testing.
-const int64_t kTestBatchSize = 1000;
+const int64_t kTestBatchSize = 128;
 
 // The number of epochs to train.
-const int64_t kNumberOfEpochs = 25;
+const int64_t kNumberOfEpochs = 50;
 
 // After how many batches to log a new update with the loss value.
-const int64_t kLogInterval = 10;
+const int64_t kLogInterval = 16;
+
+// the sample vgg16 model 
+/* the implementation of vgg16 in pytorch 
+class VGG16(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG16, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, num_classes))
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+*/
+// start of the network, VGG16 
 
 struct Net : torch::nn::Module {
   Net()
-      : conv1(torch::nn::Conv2dOptions(1, 10, /*kernel_size=*/5)),  //(in_channel, out_channel, kernel_size)
-        conv2(torch::nn::Conv2dOptions(10, 20, /*kernel_size=*/5)),
-        conv3(torch::nn::Conv2dOptions(20,30,/*kernel_size=*/5)),  // added the layer
-        conv4(torch::nn::Conv2dOptions(30,40,/*kernel_size=*/5)),  //added the layer
-        fc1(320, 50),
-        fc2(50, 20), 
-        fc3(20,10){
+      : conv1(torch::nn::Conv2dOptions(1, 64, 3)),  //(in_channel, out_channel, kernel_size)
+        conv2(torch::nn::Conv2dOptions(64, 20, 5)),
+        conv3(torch::nn::Conv2dOptions(64,64,3)),  // added the layer
+        conv4(torch::nn::Conv2dOptions(128,128,3)),  //added the layer
+        conv5(torch::nn::Conv2dOptions(128,256,3)),  //added the layer
+        conv6(torch::nn::Conv2dOptions(256,256,3)),  //added the layer
+        conv7(torch::nn::Conv2dOptions(256,256,3)),  //added the layer
+        conv8(torch::nn::Conv2dOptions(256,512,3)),  //added the layer
+        conv9(torch::nn::Conv2dOptions(512,512,3)),  //added the layer
+        conv10(torch::nn::Conv2dOptions(512,512,3)),  //added the layer
+        conv11(torch::nn::Conv2dOptions(512,512,3)),  //added the layer
+        conv12(torch::nn::Conv2dOptions(512,30,5)),  //added the layer
+        conv13(torch::nn::Conv2dOptions(30,40,5)),  //added the layer
+        fc1(320,50),
+        fc2(50,40),
+        fc3(40,20), 
+        fc4(20,10){
     register_module("conv1", conv1);
     register_module("conv2", conv2);
     register_module("conv3", conv3);  //add the layer 
     register_module("conv4", conv4);   // add the layer 
+    register_module("conv6", conv6);   // add the layer 
+    register_module("conv7", conv7);   // add the layer 
+    register_module("conv8", conv8);   // add the layer 
+    register_module("conv9", conv9);   // add the layer 
+    register_module("conv10", conv10);   // add the layer 
+    register_module("conv11", conv11);   // add the layer 
+    register_module("conv12", conv12);   // add the layer
+    register_module("conv13", conv13);   // add the layer      
     register_module("conv2_drop", conv2_drop);
     register_module("fc1", fc1);
     register_module("fc2", fc2);
@@ -49,21 +172,33 @@ struct Net : torch::nn::Module {
         torch::max_pool2d(conv2_drop->forward(conv2->forward(x)), 2));
     x = x.view({-1, 320});
     x = torch::relu(fc1->forward(x));
-    x = torch::dropout(x, /*p=*/0.25, /*training=*/is_training());
+    x = torch::dropout(x, 0.25,is_training());
     x = fc2->forward(x);
     x = fc3->forward(x);
-    return torch::log_softmax(x, /*dim=*/1);
+    x = fc4->forward(x);
+    return torch::log_softmax(x,1);
   }
 
   torch::nn::Conv2d conv1;
   torch::nn::Conv2d conv2;
   torch::nn::Conv2d conv3;  // register the layer 
   torch::nn::Conv2d conv4;  //register the layer 
+  torch::nn::Conv2d conv5;  //register the layer 
+  torch::nn::Conv2d conv6;  //register the layer 
+  torch::nn::Conv2d conv7;  //register the layer 
+  torch::nn::Conv2d conv8;  //register the layer 
+  torch::nn::Conv2d conv9;  //register the layer 
+  torch::nn::Conv2d conv10;  //register the layer 
+  torch::nn::Conv2d conv11;  //register the layer 
+  torch::nn::Conv2d conv12;  //register the layer 
+  torch::nn::Conv2d conv13;  //register the layer
   torch::nn::Dropout2d conv2_drop;
   torch::nn::Linear fc1;
   torch::nn::Linear fc2;
   torch::nn::Linear fc3;
+  torch::nn::Linear fc4;
 };
+
 
 template <typename DataLoader>
 void train(
