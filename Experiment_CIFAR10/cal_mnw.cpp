96f79626142b1d14196f1fe2cd65c99d71d81282
Kazi Mejbaul Islam <222mzb@gmail.com>
2022-12-02 21:45:38 -0500

    CIFAR10 exp upload

---
+++ Experiment_CIFAR10/cal_mnw.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+
+
+    std::ifstream fileRead("CIFAR10_test_ground_truth.dat", std::ios::binary);
+    std::vector<int> load;
+    int temp;
+    while(fileRead.read((char*)&temp, sizeof(int)))
+        load.push_back(temp);
+    fileRead.close();
+
+    std::ifstream fileRead2("CIFAR10_test_prediction.dat", std::ios::binary);
+    std::vector<int> pred;
+    int temp2;
+    while(fileRead2.read((char*)&temp2, sizeof(int)))
+        pred.push_back(temp2);
+    fileRead2.close();
+
+    int count = 0;
+    for(int i = 0; i < load.size(); i++) 
+    {
+
+        if (load[i] != pred[i])
+            count +=1;        
+    }
+
+    float mnew = count/1000.0;
+
+    std::cout << "M_new: " << mnew << endl;
+
+    return 0;
+}
